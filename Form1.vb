1dd872e03a9d5b5cbca20ffa17cc24c86a4766a6
77idah <idahdorcas@gmail.com>
2025-02-21 17:12:44 +0300

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("object Demonstration")
+    End Sub
+    Private Sub Button2_click(sender As Object, e As EventArgs) Handles Button2.click
+        MsgBox("object Demonstraton")
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Application Exit")
+    End Sub
+End Class
